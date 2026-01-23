cbe6ae004f3343c480f27957208089f36214cff2
KindMinotaur <shaun.belknap@gmail.com>
2026-01-23 11:34:43 -0600

    Commiting before device update

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,47 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/KindMinotaur/httpfromtcp/internal/request"
 )
-
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	linesChannel := make(chan string, 1)
-
-	go func() {
-		defer f.Close()
-		defer close(linesChannel)
-
-		str := ""
-
-		for {
-			data := make([]byte, 8)
-			n, err := f.Read(data)
-			if err != nil {
-				break
-			}
-
-			data = data[:n]
-			if i := bytes.IndexByte(data, '\n'); i != -1 {
-				str += string(data[:i])
-				data = data[i+1:]
-				linesChannel <- str
-				str = ""
-			}
-
-			str += string(data)
-		}
-
-		if len(str) != 0 {
-			linesChannel <- str
-		}
-	}()
-
-	return linesChannel
-}
 
 func main() {
 	listener, err := net.Listen("tcp", ":42069")
@@ -54,9 +19,14 @@ func main() {
 		if err != nil {
 			log.Fatal("error", "error", err)
 		}
-		for line := range getLinesChannel(conn) {
-			fmt.Printf("read: %s\n", line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatal("error", "error", err)
 		}
 
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 	}
 }
